f7e6828a8e9777a87c5429fe0f11de129faf53c6
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2024-12-18 16:28:23 +0100

    added home button

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import React from "react";
+
+import './App.css';
+import ButtonHome from '../components/ButtonHome/ButtonHome';
+import Panneau from '../components/Panneau/Panneau'
 
 function App() {
   return (
     <>
-      
+      <ButtonHome />
+      <Panneau />
     </>
   )
 }
