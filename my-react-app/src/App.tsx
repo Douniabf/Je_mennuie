7fd2cc52ff9b7f6e4dc35dd22403e7bdcde62517
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2024-12-19 11:53:22 +0100

    added button recharge and section meme

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import ButtonHome from '../components/ButtonHome/ButtonHome';
 import Panneau from '../components/Panneau/Panneau'
+import SectionMeme from '../components/SectionMeme/SectionMeme'
+import ButtonRecharge from '../components/ButtonRecharge/ButtonRecharge';
+import axios from 'axios';
 
 function App() {
-  return (
+  return(
     <>
-      <ButtonHome />
-      <Panneau />
+      <SectionMeme />
     </>
-  )
+  );
 }
 
 export default App
