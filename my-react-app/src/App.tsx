b70bfdaf45f67e19358b5d799db59b365d301531
mguittari <mattias.guittari@gmail.com>
2024-12-26 19:54:51 +0100

    panneau

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 
-import ButtonHome from "../components/ButtonHome/ButtonHome";
+import DisplayPoem from "./Pages/DisplayPoem";
 import Panneau from "../components/Panneau/Panneau";
 
 function App() {
 	return (
 		<>
-			<ButtonHome />
+			<Panneau>
+				<DisplayPoem />
+			</Panneau>
 		</>
 	);
 }
