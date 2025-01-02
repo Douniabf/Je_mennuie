4e36151981600936d4b6830d3e49108fce2f5447
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2025-01-02 13:37:21 +0100

    section meme

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
-import ButtonHome from '../components/ButtonHome/ButtonHome';
-import Panneau from '../components/Panneau/Panneau'
-import SectionMeme from '../components/SectionMeme/SectionMeme'
-import ButtonRecharge from '../components/ButtonRecharge/ButtonRecharge';
+import ButtonHome from './components/ButtonHome/ButtonHome';
+import Panneau from './components/Panneau/Panneau'
+import SectionMeme from './components/SectionMeme/SectionMeme'
+import ButtonRecharge from './components/ButtonRecharge/ButtonRecharge';
 import axios from 'axios';
 
 
