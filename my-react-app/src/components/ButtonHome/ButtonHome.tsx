611ec4241bed37435bd46f5f4e1b38ffa33226fd
Douniabf <bouhayouf-dounia@hotmail.com>
2025-01-07 16:23:11 +0100

    delete animation

--- my-react-app/src/components/ButtonHome/ButtonHome.tsx
+++ my-react-app/src/components/ButtonHome/ButtonHome.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import Panneau from "../Panneau/Panneau";
 import "./ButtonHome.css";
 
 export default function ButtonHome() {
 	return (
-				<Link to = "/home" className="circle">
-					<span className="text">Je m'ennuie</span>
-				</Link>
+		<Link to = "/home" className="circle">
+			<span className="text">Je m'ennuie</span>
+		</Link>
 	);
 }
+
