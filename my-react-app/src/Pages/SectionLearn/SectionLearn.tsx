4a200fe318f12e9125223e6e149f09b87ebd7c8b
Douniabf <bouhayouf-dounia@hotmail.com>
2025-01-08 10:37:25 +0100

    resolve error panneau

--- my-react-app/src/Pages/SectionLearn/SectionLearn.tsx
+++ my-react-app/src/Pages/SectionLearn/SectionLearn.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import ButtonRecharge from "../../components/ButtonRecharge/ButtonRecharge.js";
-import ButtonRetour from "../../components/ButtonRetour/ButtonRetour.js";
 import Panneau from "../../components/Panneau/Panneau.js";
 import "./SectionLearn.css";
+import ButtonRecharge from "../../components/ui/ButtonRecharge/ButtonRecharge.js";
+import ButtonRetour from "../../components/ui/ButtonRetour/ButtonRetour.js";
 
 const PageLearn = () => {
   const [page, setPage] = useState(null);
